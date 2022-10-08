eadc6667a1c92757a277f4116351ba2752070998
glebite <glebite@gmail.com>
2022-10-08 17:54:14 -0400

    Updatespalooza!

--- src/data_puller.py
+++ src/data_puller.py
@@ -1,33 +1,58 @@
 import csv
 import requests
 from multiprocessing import Pool
+from dataclasses import dataclass, field
+from collections import Counter
+
+
+@dataclass
+class SiteStatus:
+    link:    list = field(default_factory=list)
+    blocked: bool = False 
+    okay:    bool = False
+    other:   bool = False
+    errors:  bool = False
 
 
 class DataPuller:
     def __init__(self, file_name):
         self.file_name = file_name
         self.data = []
-
+        self.read()
+        
     def read(self): 
         with open(self.file_name, 'r') as file:
             reader = csv.reader(file, delimiter=';')
             for row in reader:
                 self.data.append(row)
 
-def checkurl(line):
-    try:
-        r = requests.get(line[-1], timeout=10)
-    except:
-        print(f'URL error: {line[0]:60s} {line[-1]}')
-    else:
-        pass
-    
-def main():
-    x = DataPuller('../data/university_list.csv')
-    x.read()
-    p = Pool(processes=20)
-    result = p.map(checkurl, x.data)
+    def checkurl(self, line):
+        """multiprocessing worker function
+        """
+        status = SiteStatus()
+        status.link = line
+        try:
+            r = requests.get(line[-1], timeout=30)
+            if r.status_code == 403:
+                status.blocked = True
+            elif r.status_code == 200:
+                status.okay = True
+            else:
+                status.other = True
+        except:
+            status.errors = True
+        return status
 
+    def process_presence_count(self):
+        presence_counter = Counter()
+        processing_pool = Pool(processes=20)
+        results = processing_pool.map(self.checkurl, self.data)
+        for thing in results:
+            count_struct = {k: v for k, v in thing.__dict__.items() if k != 'link'}
+            presence_counter.update(count_struct)
+        return(presence_counter)
 
 if __name__ == "__main__":
-    main()
+    pulled_data = DataPuller('../data/university_list.csv')
+    presence_counter = pulled_data.process_presence_count()
+    print(presence_counter)
