308ac8ebcdc71d6feda6479545525927f33b4617
Glebite <glebite@gmail.com>
2022-10-10 13:37:36 -0400

    Cleaned up to only graph the last 32 samples (8 hours)

--- src/graph.py
+++ src/graph.py
@@ -4,9 +4,8 @@ plt.rcParams["figure.figsize"] = [7.00, 3.50]
 plt.rcParams["figure.autolayout"] = True
 df = pd.read_csv("../data.txt", header=None)
 df.columns = ['Date', 'Okay', 'Blocked', 'Down', 'Other']
-print(type(df))
-print(df)
-df.plot(x='Date', y = ['Okay', 'Blocked', 'Down', 'Other'])
+partial_df = df.tail(32)
+partial_df.plot(x='Date', y = ['Okay', 'Blocked', 'Down', 'Other'])
 plt.grid()
 plt.title('Criteria over time')
 plt.ylabel('Criteria count')
