76421ab6797feccea4ef0af7a329db3a25ffebd6
Glebite <glebite@gmail.com>
2022-10-15 07:50:50 -0400

    Updated output of min/max for recent data and historical

--- src/graph.py
+++ src/graph.py
@@ -1,16 +1,23 @@
 """
 """
 import time
-offset = time.timezone if (time.localtime().tm_isdst == 0) else time.altzone
-offset = offset / 60 / 60 * -1
 import pandas as pd
 from matplotlib import pyplot as plt
+
+def extract_min_max(df):
+    df2 = partial_df.loc[partial_df['Down'].idxmax()]
+    df3 = partial_df.loc[partial_df['Down'].idxmin()]
+    max = df2['Down']
+    min = df3['Down']
+    return min, max
+
+offset = time.timezone if (time.localtime().tm_isdst == 0) else time.altzone
+offset = offset / 60 / 60 * -1
 plt.rcParams["figure.figsize"] = [7.00, 3.50]
 plt.rcParams["figure.autolayout"] = True
 df = pd.read_csv("../data.txt", header=None)
 df.columns = ['Date', 'Okay', 'Blocked', 'Down', 'Other']
 partial_df = df.tail(32)
-print(partial_df.tail(1))
 partial_df.plot(x='Date', y = ['Okay', 'Blocked', 'Down', 'Other'])
 plt.grid()
 plt.title('Criteria over time')
@@ -18,3 +25,9 @@ plt.ylabel('Criteria count')
 plt.xlabel(f'Time (GMT{offset})')
 plt.xticks(rotation=90)
 plt.savefig('graph.png')
+min, max = extract_min_max(partial_df)
+print(f'Recent    : {min=} {max=}')
+min, max = extract_min_max(df)
+print(f'Historical: {min=} {max=}')
+
+
